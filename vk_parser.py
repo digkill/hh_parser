27dedffdce4a32e2d28b687983851ee824ff6f10
Edifanoff <vitaliy@edifanov.com>
2019-11-03 03:12:36 +0300

    add vk parser

---
+++ vk_parser.py
@@ -0,0 +1,48 @@
+import requests
+import time
+import csv
+
+
+
+def takePosts():
+    token = '03cce87403cce87403cce8744003a12cdb003cc03cce8745e72fcb5e11d83c9be317fc6'
+    version = 5.103
+    domain = 'papclub1'
+    offset = 0
+    count = 100
+    allPost = []
+
+
+    while offset < 1000:
+        response = requests.get('https://api.vk.com/method/wall.get',
+                            params={
+                                'access_token': token,
+                                'v': version,
+                                'domain': domain,
+                                'count': count,
+                                'offset': offset
+                            })
+
+        data = response.json()['response']['items']
+        offset +=100
+        allPost.extend(data)
+        time.sleep(0.5)
+    return allPost
+
+def fileWriter(allPosts):
+    with open('posts.csv', 'w', encoding='utf-8') as file:
+        line = csv.writer(file)
+        line.writerow(('likes', 'body', 'url'))
+        for post in allPosts:
+            try:
+                if post['attachments'][0]['type']:
+                    imgUrl = post['attachments'][0]['photo']['sizes'][-1]['url']
+                else:
+                    imgUrl = 'pass'
+            except:
+                pass
+            line.writerow((post['likes']['count'], post['text'], imgUrl))
+
+
+allPosts = takePosts()
+fileWriter(allPosts)
